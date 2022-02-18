536d64072ac08eb4b0324f219a4815635e0595bd
Nikotiine <niko.tiine@gmail.com>
2022-02-18 18:20:28 +0100

    multiselect addvoie ok

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import "../src/assets/style/main.scss";
 import "../axios";
 //import axios from "axios";
